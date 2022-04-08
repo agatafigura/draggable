19ecda51b611030fa0f4d7cb5c63361adc3a39cd
agatafigura <83277815+agatafigura@users.noreply.github.com>
2022-04-08 13:23:13 +0200

    changed colors

--- src/App.vue
+++ src/App.vue
@@ -15,37 +15,103 @@
 
 .row {
   display: flex;
-  justify-content: space-around;
-  padding-top: 5rem;
-  margin-left: 20rem;
-  margin-right: 20rem;
+  column-gap: 1rem;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 
 .list-group {
   width: 15rem;
   display: flex;
   flex-direction: column;
-  row-gap: 0.1rem;
+  row-gap: 0.3rem;
+  margin-bottom: 0.5rem;
+  min-height: 18rem;
 }
 
 .list1 .list-group-item {
-  background-color: lightblue;
-  border: solid 0.5px rgb(106, 164, 184);
+  /* background-color: lightblue; */
+  border: solid 0.5px rgb(243, 243, 155);
+  border-left-width: 0.5rem;
 }
 
 .list2 .list-group-item {
-  background-color: lightgreen;
-  border: solid 0.5px rgb(93, 189, 93);
+  border: solid 0.5px lightblue;
+  border-left-width: 0.5rem;
+}
+
+.list3 .list-group-item {
+  border: solid 0.5px lightgreen;
+  border-left-width: 0.5rem;
+}
+
+.list4 .list-group-item {
+  border: solid 0.5px lightcoral;
+  border-left-width: 0.5rem;
 }
 
 h3 {
-  text-align: left;
+  text-align: center;
+  padding: 0.5rem;
+  margin: 0;
 }
 
+#list1 h3 {
+  background-color: rgb(243, 243, 155);
+ }
+
+ #list2 h3 {
+   background-color: lightblue;
+ }
+
+ #list3 h3 {
+   background-color: lightgreen;
+ }
+
+ #list4 h3 {
+   background-color: lightcoral;
+ }
+
 .list-group-item {
-  padding: 0.5rem;
+  margin-left: 0.5rem;
+  margin-right: 0.5rem;
   /* border: solid 0.5px gray; */
   text-align: left;
+  padding: 0.5rem;
+}
+
+#list1 {
+  border: rgb(211, 211, 44) 1px solid;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  row-gap: 1rem;
+}
+
+#list2 {
+   border: rgb(96, 187, 218) 1px solid;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  row-gap: 1rem;
+}
+
+#list3 {
+   border: rgb(41, 196, 41) 1px solid;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  row-gap: 1rem;
+}
+
+#list4 {
+   border: rgb(240, 76, 76) 1px solid;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  row-gap: 1rem;
 }
 
 #nav {
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,11 +1,36 @@
 <template>
   <div class="row">
-    <div class="col-3">
-      <h3>To do</h3>
-      <draggable class="list-group list1" :list="list1" key="":group="{ name: 'people', pull: 'clone', put: false }" @change="log">
+    <div class="col-3" id="list1">
+      <h3>Tasks</h3>
+      <draggable
+        class="list-group list1"
+        :list="list1"
+        :group="{ name: 'tasks', pull: 'clone', put: false }"
+        @change="log"
+      >
         <div
           class="list-group-item"
           v-for="element in list1"
+          :key="element.name"
+        >
+
+          {{ element.name }}
+        </div>
+        
+      </draggable>
+    </div>
+
+    <div class="col-3" id="list2">
+      <h3>To do</h3>
+      <draggable
+        class="list-group list2"
+        :list="list2"
+        group="tasks"
+        @change="log"
+      >
+        <div
+          class="list-group-item"
+          v-for="element in list2"
           :key="element.name"
         >
           {{ element.name }}
@@ -13,12 +38,37 @@
       </draggable>
     </div>
 
-    <div class="col-3">
-      <h3>Done</h3>
-      <draggable class="list-group list2" :list="list2" group="people" @change="log">
+    <div class="col-3" id="list3">
+      <h3>Doing</h3>
+      <draggable
+        class="list-group list3"
+        :list="list3"
+        group="tasks"
+        @change="log"
+      >
         <div
           class="list-group-item"
-          v-for="element in list2"
+          v-for="element in list3"
+          :key="element.name"
+        >
+        
+          {{ element.name }}
+        </div>
+      </draggable>
+    </div>
+   
+
+    <div class="col-3" id="list4">
+      <h3>Done</h3>
+      <draggable
+        class="list-group list4"
+        :list="list4"
+        group="tasks"
+        @change="log"
+      >
+        <div
+          class="list-group-item"
+          v-for="element in list4"
           :key="element.name"
         >
           {{ element.name }}
@@ -38,7 +88,7 @@ export default {
   display: "Clone",
   order: 2,
   components: {
-    draggable
+    draggable,
   },
 
   data() {
@@ -47,19 +97,20 @@ export default {
         { name: "Wash dishes", id: 1 },
         { name: "Clean the kitchen", id: 2 },
         { name: "Send an email", id: 3 },
-        { name: "Mop the floors", id: 4 }
-      ],
-      list2: [
-        { name: "Walk the dog", id: 5 },
+        { name: "Mop the floors", id: 4 },
+         { name: "Walk the dog", id: 5 },
         { name: "Water the plants", id: 6 },
-        { name: "Make dinner", id: 7 }
-      ]
+        { name: "Make dinner", id: 7 },
+      ],
+      list2: [],
+      list3: [],
+      list4: [],
     };
   },
-   methods: {
-    log: function(evt) {
+  methods: {
+    log: function (evt) {
       window.console.log(evt);
-    }
-  }
+    },
+  },
 };
 </script>
